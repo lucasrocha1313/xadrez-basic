1a31152ebb3dc01fad22ee44ca00adfd32f630c9
Tonim <lucasrocha1313@gmail.com>
2020-04-11 16:03:47 -0300

    trantando excecoes no tabuleiro

--- xadrez-basic/tabuleiro/Tabuleiro.cs
+++ xadrez-basic/tabuleiro/Tabuleiro.cs
@@ -20,10 +20,43 @@ namespace tabuleiro
             return Pecas[linha, coluna];
         }
 
+        public Peca ObterPeca(Posicao posicao)
+        {
+            return Pecas[posicao.Linha, posicao.Coluna];
+        }
+
         public void ColocarPeca(Peca peca, Posicao posicao)
         {
+            if(ExistePeca(posicao))
+            {
+                throw new TabuleiroException("Já existe uma peça nesta posição");
+            }
             Pecas[posicao.Linha, posicao.Coluna] = peca;
             peca.Posicao = posicao;
+        }
+
+        public bool ExistePeca(Posicao posicao)
+        {
+            ValidarPosicao(posicao);
+            return ObterPeca(posicao) != null;
+        }
+        public bool PosicaoValida(Posicao posicao)
+        {
+            if(posicao.Linha < 0 || posicao.Linha > Linhas
+                || posicao.Coluna < 0 || posicao.Coluna > Colunas)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao posicao)
+        {
+            if(!PosicaoValida(posicao))
+            {
+                throw new TabuleiroException("Posição Inválida");
+            }
         }
     }
 }
